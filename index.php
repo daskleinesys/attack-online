b5f52cb44d3d3cf875a57dfdcbed9a99ae6728de
Tom Schagerl <tom@thespielplatz.com>
2016-01-16 18:14:03 +0100

    added basic log4php setup

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <?php
 namespace AttOn;
 
-// LOADING FRAMEWORKS
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'defines' . DIRECTORY_SEPARATOR . 'defines.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'defines' . DIRECTORY_SEPARATOR . 'gamevars.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'defines' . DIRECTORY_SEPARATOR . 'local_configuration.php';
-
-// SETUP TIMEZONE
-date_default_timezone_set('Europe/Vienna');
-
-// DEBUG
+// DEBUG && NO-MERGE
 $debug = isset($_REQUEST['debug']) ? true : false;
 if ($debug) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 }
-
-// NO-MERGE
 $nomerge = isset($_REQUEST['no_merge']) || isset($_REQUEST['nomerge']) ? true : false;
+
+// SETUP TIMEZONE
+date_default_timezone_set('Europe/Vienna');
+
+// LOADING FRAMEWORKS
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+//require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'Tools' . DIRECTORY_SEPARATOR . 'AttonAutoloader.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'defines' . DIRECTORY_SEPARATOR . 'defines.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'defines' . DIRECTORY_SEPARATOR . 'gamevars.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'defines' . DIRECTORY_SEPARATOR . 'local_configuration.php';
+
+// INIT ERROR-HANDLING && LOGGING
+\Logger::configure($log4php_config);
+$logger = \Logger::getLogger('index.php');
 
 // INIT SLIM APP
 $app = new \Slim\Slim( array('debug' => $debug, 'view' => new \Slim\Views\Twig()));
@@ -26,13 +29,23 @@ $view = $app->view();
 $view->parserExtensions = array(new \Twig_Extension_Debug());
 $view->parserOptions = array('debug' => $debug);
 
-// DEFINE ERROR HANDLING
+// DEFINE SLIM-ERROR HANDLING
 $app->error(function(\Exception $e) use ($app) {
+    $logger->error($e->getMessage());
     $app->render('error.twig');
 });
 $app->notFound(function() use ($app) {
     $app->render('404.twig');
 });
+
+// INIT DB-CONNECTION
+try {
+	SQLCommands::init();
+} catch (DataSourceException $e) {
+    $logger->fatal($e->getMessage());
+    $app->render('error.twig');
+    die();
+}
 
 // DEFINE GET ROUTES
 include_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'get.php';
--- php/defines/local_configuration_sample.php
+++ php/defines/local_configuration_sample.php
@@ -6,3 +6,5 @@ define('DB_PASSWORD', 'password');
 define('DB_NAME', 'database');
 
 define('ABS_REF_PREFIX', '/');
+
+$log4php_config = array();
