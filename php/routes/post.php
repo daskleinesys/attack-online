46c47469b23cf2c9e05fa817c58cc8f6f4af8040
Tom Schagerl <tom@thespielplatz.com>
2016-01-21 17:19:43 +0100

    added login post route

--- php/routes/post.php
+++ php/routes/post.php
@@ -1,1 +1,12 @@
 <?php
+namespace AttOn;
+
+$app->post('/login/', function() use ($app, $debug) {
+
+    // TODO : check if correct login data
+
+    // TODO : on error post error + main page
+
+    // successfully logged in, redirect to main route
+    $app->redirect(ABS_REF_PREFIX);
+});
