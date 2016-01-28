b1e9dfa86df4a2d67cb76484f8dcce2df588329d
Tom Schagerl <tom@thespielplatz.com>
2016-01-28 18:34:27 +0100

    checksessions cookie check bugfix, added favicon

--- php/classes/Tools/CheckSessions.class.php
+++ php/classes/Tools/CheckSessions.class.php
@@ -6,7 +6,7 @@ use AttOn\Exceptions;
 class CheckSessions {
 
 	public static function checkCookies() {
-		if (isset($_SESSION['user_id']) && intval($_SESSION['user_id']) > 0) {
+		if (isset($_SESSION['user_id']) && (int)$_SESSION['user_id'] > 0) {
             ModelUser::setCurrentUser($_SESSION['user_id']);
             return true;
         }
@@ -14,8 +14,8 @@ class CheckSessions {
             return false;
         }
         try {
-            $user_id = ModelUser::loginWithToken($_COOKIE['user_token']);
-            $_SESSION['user_id'] = $user_id;
+            $_User = ModelUser::loginWithToken($_COOKIE['user_token']);
+            $_SESSION['user_id'] = $_User->getId();
         } catch (Exceptions\LoginException $ex) {
             return false;
         }
