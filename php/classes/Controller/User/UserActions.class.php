6f048e9dd8a271bd569199cf7f67b72fed0a2888
Tom Schagerl <tom@thespielplatz.com>
2016-01-28 18:50:54 +0100

    useractions updated logout, nav template update

--- php/classes/Controller/User/UserActions.class.php
+++ php/classes/Controller/User/UserActions.class.php
@@ -157,13 +157,9 @@ class UserActions {
      */
     public static function logout() {
         $_SESSION = array();
-
-        // also, delete cookies
-        // TODO : do not save user_id in $_COOKIE
-        echo 'TODO : do not save user_id in $_COOKIE';
-        if (isset($_COOKIE['user_id'])) {
+        if (isset($_COOKIE['user_token'])) {
             // Delete the user ID and username cookies by setting their expirations to an hour ago (3600)
-            setcookie('user_id', '', time() - 3600);
+            setcookie('user_token', '', time() - 3600, ABS_REF_PREFIX);
         }
     }
 
