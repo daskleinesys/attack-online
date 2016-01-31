813e0a770d9fa42b1b7879b2eb854e21c05e1bcf
Tom Schagerl <tom@thespielplatz.com>
2016-01-31 13:21:45 +0100

    generic :content route cleanup

--- php/routes/get.php
+++ php/routes/get.php
@@ -20,62 +20,41 @@ $app->get('/logout/', function() use ($app, $debug) {
 });
 
 $app->get('/:content/', function($content) use ($app, $debug, $logger) {
-    // factory pattern
-    try {
-        $env = $app->environment();
-        $factories = Tools\Autoloader::loadFactories($env['basepath'] . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Content' . DIRECTORY_SEPARATOR . 'Factories' . DIRECTORY_SEPARATOR, '\\AttOn\\View\\Content\\Factories\\');
-    } catch (\Exception $ex) {
-        if ($debug) {
-            throw $ex;
-        }
-        $logger->error($ex);
-        $data = array();
-        $data['user'] = Model\User\ModelUser::getCurrentUser()->getViewData();
-        $data['errors'] = array(
-            'message' => 'Unable to load content. Please contact a site-admin.'
-        );
-        $app->render('error.twig', $data);
-        return;
-    }
-    // TODO : make getoperation possible
-    echo 'TODO : make getoperation possible';
-    die('');
+    $data = array();
 
+    // factory pattern
+    $env = $app->environment();
+    $factories = Tools\Autoloader::loadFactories($env['basepath'] . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Content' . DIRECTORY_SEPARATOR . 'Factories' . DIRECTORY_SEPARATOR, '\\AttOn\\View\\Content\\Factories\\');
+
+    // get operation
     foreach ($factories as $factory) {
-        if ($factory->getName() == $content) {
+        if ($factory->getName() === $content) {
             try {
-                $content_object = $factory->getOperation($this->id_user,$this->id_game);
-            } catch (SessionException $ex) {
+                $content_object = $factory->getOperation();
+            } catch (Exceptions\SessionException $ex) {
                 $logger->error($ex);
-                $session_error = true;
-                $content_object = new ContentHome($this->id_user,$this->id_game,'home', CHECK_SESSION_NONE);
-                $error_msg[] = 'Sorry, you have no permission to view this site.';
-            } catch (Exception $ex) {
-                $logger->error($ex);
-                $error_msg[] = 'Unable to load content. Please contact a site-admin.';
+                $data['user'] = Model\User\ModelUser::getCurrentUser()->getViewData();
+                $data['errors'] = array(
+                    'message' => $ex->getMessage()
+                );
+                $app->render('error.twig', $data);
             }
         }
     }
 
-    // TODO : rework render 404.twig
+    // render 404
     if (!isset($content_object)) {
-        $content_object = new ContentPageNotFound($this->id_user,$this->id_game,'pagenotfound',CHECK_SESSION_NONE);
+        $app->notFound();
     }
 
-    // TODO : rework -> show error page
-    // throw exception if no content is created
-    if (!is_object($content_object)) {
-        $logger->fatal('No Operation created!');
-        throw new Exception('No Operation created!');
-    }
-
-    $data = array();
+    // run operation
+    $content_object->run($data);
     $data['user'] = Model\User\ModelUser::getCurrentUser()->getViewData();
-    $data['template'] = 'games';
     $app->render('main.twig', $data);
 });
 
 /*
+TODO Werner? concept + proposol for the following:
 $app->get('/:content/')function($content) {
     new ContentFactory();
     $controller = ContentFactory->getController($content);
