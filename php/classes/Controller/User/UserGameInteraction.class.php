372407c4fcb40ce88c498294fc548f6886d7eb55
Tom Schagerl <tom@thespielplatz.com>
2016-02-01 14:38:59 +0100

    refactored UserGameInteraction

--- php/classes/Controller/User/UserGameInteraction.class.php
+++ php/classes/Controller/User/UserGameInteraction.class.php
@@ -1,75 +1,96 @@
 <?php
 namespace AttOn\Controller\User;
+use AttOn\Controller\Interfaces\ConstrictedController;
+use Logger;
+use AttOn\Model\Game\ModelGame;
+use AttOn\Exceptions\JoinUserException;
+use AttOn\Exceptions\ControllerException;
+use AttOn\Model\User\ModelIsInGameInfo;
+use AttOn\Model\User\ModelUser;
 
 class UserGameInteraction extends ConstrictedController {
-    private $_Logger;
 
-    /**
-     * @param int $id_user - id of the user accessing the moderation actions
-     */
-    public function __construct($id_user) {
-        parent::__construct($id_user);
-        $this->_Logger = Logger::getLogger('UserGameInteraction');
+    private $logger;
+
+    public function __construct() {
+        $this->logger = Logger::getLogger('UserGameInteraction');
     }
 
     /**
      * tries to join a game with the user
+     *
      * @param int $id_color
      * @param int $id_game
      * @param string $password
-     * @throws JoinUserException,NullPointerException
+     * @throws JoinUserException, NullPointerException
+     * @return boolean
      */
     public function join($id_color = null, $id_game, $password = null) {
         $id_color = intval($id_color);
         $id_game = intval($id_game);
-
-        $_Game = ModelGame::getGame($id_game);
+        $game = ModelGame::getGame($id_game);
 
         // check password
-        if ($_Game->checkPasswordProtection()) if (!$_Game->checkPassword($password)) throw new JoinUserException('Invalid password.');
-        if (ModelIsInGameInfo::isUserInGame($this->id_user, $id_game)) throw new JoinUserException('User already in this game!');
-        if ($_Game->getFreeSlots() <= 0) throw new JoinUserException('Game is full.');
+        if ($game->checkPasswordProtection() && !$game->checkPassword($password)) {
+            throw new JoinUserException('Invalid password.');
+        }
+        if ($this->checkInGame($id_game)) {
+            throw new JoinUserException('User already in this game!');
+        }
+        if ($game->getFreeSlots() <= 0) {
+            throw new JoinUserException('Game is full.');
+        }
 
         // join game
-        ModelIsInGameInfo::joinGame($this->id_user, $id_game, $id_color);
+        ModelIsInGameInfo::joinGame(ModelUser::getCurrentUser()->getId(), $id_game, $id_color);
+        return true;
     }
 
     /**
      * trys to leave the given game, returns true on success
+     *
      * @param int $id_game
-     * @throws ControllerException
+     * @throws ControllerException, NullPointerException
      * @return boolean
      */
     public function leaveGame($id_game) {
         $id_game = intval($id_game);
-        try {
-            $_Game = ModelGame::getGame($id_game);
-        } catch (NullPointerException $ex) {
-            throw new ControllerException('Game not found.');
+        $game = ModelGame::getGame($id_game);
+
+        if ($game->getStatus() !== GAME_STATUS_NEW) {
+            throw new ControllerException('Can\'t leave game. It has allready started.');
+        }
+        if ($game->checkProcessing()) {
+            throw new ControllerException('Can\'t leave game. It has allready started.');
         }
 
-        if ($_Game->getStatus() != 'new') throw new ControllerException('Can\'t leave game. It has allready started.');
-        if ($_Game->checkProcessing()) throw new ControllerException('Can\'t leave game. It has allready started.');
-
-        ModelIsInGameInfo::leaveGame($id_game, $this->id_user);
+        ModelIsInGameInfo::leaveGame(ModelUser::getCurrentUser()->getId(), $id_game);
         return true;
     }
 
     /**
      * selects a game and throws exception if game is not found
+     *
      * @param int $id_game
      * @throws NullPointerException, ControllerException
-     * @return int id_game
+     * @return boolean
      */
     public function selectGame($id_game) {
         $id_game = intval($id_game);
-        if (!$this->checkInGame($id_game)) throw new ControllerException('Unable to select game as the user is not in it.');
-        $_Game = ModelGame::getGame($id_game);
-        if ($_Game->getStatus() == GAME_STATUS_NEW) throw new ControllerException('Unable to select game as it is not yet started.');
-        if ($_Game->getStatus() == GAME_STATUS_DONE) throw new ControllerException('Unable to select game as it is already done.');
+        if (!$this->checkInGame($id_game)) {
+            throw new ControllerException('User is not in this game.');
+        }
+        $game = ModelGame::getGame($id_game);
+
+        if ($game->getStatus() === GAME_STATUS_NEW) {
+            throw new ControllerException('Unable to select game as it is not yet started.');
+        }
+        if ($game->getStatus() === GAME_STATUS_DONE) {
+            throw new ControllerException('Unable to select game as it is already done.');
+        }
 
         $_SESSION['game_id'] = $id_game;
-        return $_Game->getId();
+        return true;
     }
 
 }
--- php/classes/Model/User/ModelIsInGameInfo.class.php
+++ php/classes/Model/User/ModelIsInGameInfo.class.php
@@ -1,17 +1,17 @@
 <?php
 class ModelIsInGameInfo {
-	
+
 	// list of all models
 	// $models = dict(int id_user = array(int id_game = ModelIsInGameInfo)))
 	private static $models = array();
-	
+
 	// member vars
 	private $id_user; // int
-	private $id_game; // int 
+	private $id_game; // int
 	private $id_color; // int id_color
 	private $money; // int/numer
 	private $id_set; // int id_set
-	
+
 	/**
 	 * returns the specific model
 	 * @param $id_user int
@@ -22,10 +22,10 @@ class ModelIsInGameInfo {
 	private function __construct($id_user, $id_game) {
 		$this->id_user = intval($id_user);
 		$this->id_game = intval($id_game);
-		
+
 		$this->fill_member_vars();
 	}
-	
+
 	/**
 	 * returns the specific model, if no id_game given, default rules are loaded (id_game == 0)
 	 * @param $id_user int
@@ -35,10 +35,10 @@ class ModelIsInGameInfo {
 	 */
 	public static function getIsInGameInfo($id_user, $id_game) {
 		if (isset(self::$models[$id_user][$id_game])) return self::$models[$id_user][$id_game];
-		
+
 		return self::$models[$id_user][$id_game] = new ModelIsInGameInfo($id_user,$id_game);
 	}
-	
+
 	/**
 	 * returns an iterator for all games the user is in or all users for given game
 	 * @param int $id_user (if null, query is for all)
@@ -52,23 +52,23 @@ class ModelIsInGameInfo {
 		$dict = array();
 		$dict[':id_user'] = ($id_user == null) ? '%' : intval($id_user);
 		$dict[':id_game'] = ($id_game == null) ? '%' : intval($id_game);
-		
+
 		// query iigs
 		try {
 			$result = DataSource::Singleton()->epp($query,$dict);
 		} catch (DataSourceException $ex) {
 			throw $ex;
 		}
-		
+
 		foreach ($result as $iig) {
 			$id_game = $iig['id_game'];
 			$id_user = $iig['id_user'];
 			$models[] = self::getIsInGameInfo($id_user, $id_game);
 		}
-		
+
 		return new ModelIterator($models);
 	}
-	
+
 	/**
 	 * joins the user in given game
 	 * @param int $id_user
@@ -76,11 +76,11 @@ class ModelIsInGameInfo {
 	 * @throws NullPointerException, JoinUserException
 	 * @return ModelIsInGameInfo
 	 */
-	public static function joinGame($id_user,$id_game,$id_color = null) {
+	public static function joinGame($id_user, $id_game, $id_color = null) {
 		$id_user = intval($id_user);
 		$id_game = intval($id_game);
 		if ($id_color != null) $id_color = intval($id_color);
-		
+
 		// check if user and game exist
 		try {
 			$_User = ModelUser::getUser($id_user);
@@ -88,15 +88,15 @@ class ModelIsInGameInfo {
 		} catch (Exception $ex) {
 			throw $ex;
 		}
-		
+
 		if ($_Game->getFreeSlots() <= 0) throw new JoinUserException('Game is full.');
-		
+
 		// check if user is in the game
 		$iter = ModelUser::iterator($id_game);
 		while ($iter->hasNext()) {
 			if ($iter->next() == $_User) throw new JoinUserException('User allready in this game.');
 		}
-		
+
 		// check if color is free
 		if ($id_color != null) {
 			if (!$_Game->checkIfColorIsFree($id_color)) $id_color = null;
@@ -112,59 +112,59 @@ class ModelIsInGameInfo {
 			}
 		}
 		if ($id_color == null) throw new JoinUserException('No free color found.');
-		
+
 		// insert player
 		$dict = array();
 		$dict[':id_user'] = $id_user;
 		$dict[':id_game'] = $id_game;
 		$dict[':id_color'] = $id_color;
 		DataSource::Singleton()->epp('join_game',$dict);
-		
+
 		// set user notification rules
 		ModelInGamePhaseInfo::getInGamePhaseInfo($id_user,$id_game);
-		
+
 		return self::getIsInGameInfo($id_user, $id_game);
 	}
-	
+
 	/**
 	 * deletes all models and corresponding database infos
 	 * @param $id_game int
 	 * @param $id_user int (if null all models for this game are deleted)
 	 * @return void
 	 */
-	public static function deleteIsInGameInfos($id_game,$id_user = null) {
+	public static function deleteIsInGameInfos($id_user = null, $id_game) {
 		$query = ($id_user == null) ? 'delete_iig_info_for_game' : 'delete_iig_info_for_user';
 		$dict = array(':id_game' => intval($id_game));
 		if ($id_user != null) $dict[':id_user'] = intval($id_user);
 		DataSource::getInstance()->epp($query,$dict);
-		
+
 		if ($id_user == null) {
 			foreach (array_keys(self::$models) as $key) {
 				unset(self::$models[$key][$id_game]);
-			} 
+			}
 		} else {
 			unset(self::$models[$id_user][$id_game]);
 		}
 	}
-	
+
 	/**
 	 * delete user from game (also deletes notification infos)
 	 * @param int $id_game
 	 * @param int $id_user
 	 * @return void
 	 */
-	public static function leaveGame($id_game,$id_user) {
+	public static function leaveGame($id_user, $id_game) {
 		self::deleteIsInGameInfos($id_game,$id_user);
 		ModelInGamePhaseInfo::deleteInGamePhaseInfos($id_game,$id_user);
 	}
-	
+
 	/**
 	 * checks if the given user is in the given game
 	 * @param int $id_user
 	 * @param int $id_game
 	 * @return boolean
 	 */
-	public static function isUserInGame($id_user,$id_game) {
+	public static function isUserInGame($id_user, $id_game) {
 		if (isset(self::$models[$id_user][$id_game])) return true;
 		try {
 			self::getIsInGameInfo($id_user, $id_game);
@@ -173,7 +173,7 @@ class ModelIsInGameInfo {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * changes the color if it is free
 	 * @param $id_color int
@@ -187,7 +187,7 @@ class ModelIsInGameInfo {
 		$this->id_color = $id_color;
 		DataSource::Singleton()->epp('update_user_color_for_game',array(':id_user' => $this->id_user, ':id_game' => $this->id_game, ':id_color' => $this->id_color));
 	}
-	
+
 	/**
 	 * adds the given amount to the player money (can be negative)
 	 * @param $money int/number
@@ -198,7 +198,7 @@ class ModelIsInGameInfo {
 		$this->money += $money;
 		DataSource::Singleton()->epp('set_money_for_user',array(':id_user' => $this->id_user, ':id_game' => $this->id_game, ':money' => $this->money));
 	}
-	
+
 	/**
 	 * sets the starting set for the user
 	 * @param $id_set int
@@ -211,49 +211,49 @@ class ModelIsInGameInfo {
 		$this->id_set = $id_set;
 		DataSource::Singleton()->epp('set_starting_set_for_user',array(':id_user' => $this->id_user, ':id_game' => $this->id_game, ':id_set' => $this->id_set));
 	}
-	
+
 	/**
 	 * @return int
 	 */
 	public function getIdUser() {
 		return $this->id_user;
 	}
-	
+
 	/**
 	 * @return int
 	 */
 	public function getIdGame() {
 		return $this->id_game;
 	}
-	
+
 	/**
 	 * @return int
 	 */
 	public function getIdColor() {
 		return $this->id_color;
 	}
-	
+
 	/**
 	 * @return ModelColor
 	 */
 	public function getColor() {
 		return ModelColor::getModelColor($this->id_color);
 	}
-	
+
 	/**
 	 * @return int/number
 	 */
 	public function getMoney() {
 		return $this->money;
 	}
-	
+
 	/**
 	 * @return int
 	 */
 	public function getIdStartingSet() {
 		return $this->id_set;
 	}
-	
+
 	private function fill_member_vars() {
 		$query = 'get_iig_info_for_user_in_game';
 		$dict = array();
@@ -261,7 +261,7 @@ class ModelIsInGameInfo {
 		$dict[':id_game'] = $this->id_game;
 		$result = DataSource::getInstance()->epp($query,$dict);
 		if (empty($result)) throw new NullPointerException('User is not in this game!');
-		
+
 		$data = $result[0];
 		$this->id_color = $data['id_color'];
 		$this->money = $data['money'];
