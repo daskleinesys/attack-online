42ce0fc0ebc943784d00024c9cae0b4688907b83
Tom Schagerl <tom@thespielplatz.com>
2016-04-25 17:55:06 +0200

    production class php wip

--- php/classes/View/Content/Operations/ContentProduction.class.php
+++ php/classes/View/Content/Operations/ContentProduction.class.php
@@ -22,24 +22,62 @@ class ContentProduction extends ContentOperation {
         $data['template'] = $this->getTemplate();
         $this->addCurrentGameInfo($data);
 
+        $game = ModelGame::getCurrentGame();
+        if ($game->getIdPhase() !== PHASE_PRODUCTION) {
+            $data['notCurrentPhase'] = true;
+        }
+
         $this->handleInput($data);
         $this->showMoves($data);
+        $this->showCurrentProduction($data);
 
         if (!$this->checkFixate($data, PHASE_PRODUCTION)) {
             $this->showNewMove($data);
         }
     }
 
-    private function showMoves(array &$data) {
-    }
-
-    private function showNewMove(array &$data) {
-    }
-
     private function handleInput(array &$data) {
         if (empty($_POST)) {
             return;
         }
+    }
+
+    private function showMoves(array &$data) {
+
+    }
+
+    private function showCurrentProduction(array &$data) {
+
+    }
+
+    private function showNewMove(array &$data) {
+        $areasViewData = array();
+        $areas = ModelArea::iterator(TYPE_LAND);
+        while ($areas->hasNext()) {
+            /* @var $area ModelArea */
+            $area = $areas->next();
+            $zArea = ModelGameArea::getGameAreaForArea(ModelGame::getCurrentGame()->getId(), $area->getId());
+            $areaViewData = array();
+            $areaViewData['id_zarea'] = $zArea->getId();
+            $areaViewData['number'] = $area->getNumber();
+            $areaViewData['name'] = $area->getName();
+            if ($zArea->getIdUser() === ModelUser::getCurrentUser()->getId()) {
+                $areasViewData[] = $areaViewData;
+            }
+        }
+        $data['areas'] = $areasViewData;
+
+        $unitsViewData = array();
+        $unit_iter = ModelLandUnit::iterator();
+        while ($unit_iter->hasNext()) {
+            /* @var $unit ModelLandUnit */
+            $unit = $unit_iter->next();
+            $unitsViewData[] = array(
+                'id' => $unit->getId(),
+                'name' => $unit->getName()
+            );
+        }
+        $data['units'] = $unitsViewData;
     }
 
 }
