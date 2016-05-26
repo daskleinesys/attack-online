7079315c571da0f46d3436172e70f24ff93d58ad
Tom Schagerl <tom@thespielplatz.com>
2016-05-26 15:27:19 +0200

    docker added xdebug extension

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,13 @@ RUN docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-di
     docker-php-ext-install -j$(nproc) mysqli soap gd zip pdo pdo_mysql
 RUN echo 'date.timezone = "Europe/Vienna"' > /usr/local/etc/php/php.ini
 
+# setup xdebug
+RUN yes | pecl install xdebug \
+    && echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo 'xdebug.remote_port="9000"' >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.remote_autostart=off" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo 'xdebug.idekey="PHPSTORM"' >> /usr/local/etc/php/conf.d/xdebug.ini
+
 # Configure Apache as needed
 RUN a2enmod rewrite
--- docker-setup.sh
+++ docker-setup.sh
@@ -2,28 +2,29 @@
 
 # start new docker-container with mysql database
 docker run --name attack-db -d -p 3306:3306 \
--v $(pwd)/mysqldumps:/docker-entrypoint-initdb.d \
--e MYSQL_ROOT_PASSWORD=attack \
--e MYSQL_USER=attack \
--e MYSQL_PASSWORD=attack \
--e MYSQL_DATABASE=attack \
-mariadb:latest --character-set-server=utf8 --collation-server=utf8_unicode_ci
+    -v $(pwd)/mysqldumps:/docker-entrypoint-initdb.d \
+    -e MYSQL_ROOT_PASSWORD=attack \
+    -e MYSQL_USER=attack \
+    -e MYSQL_PASSWORD=attack \
+    -e MYSQL_DATABASE=attack \
+    mariadb:latest --character-set-server=utf8 --collation-server=utf8_unicode_ci
 
 # create new docker-image for php-server
 docker build -t attack .
 
 # start new docker-container using the php-server docker-image just created
 docker run --name attack --link attack-db:mysql -d -p 80:80 \
--v $(pwd)/dist:/var/www/html/dist \
--v $(pwd)/php:/var/www/html/php \
--v $(pwd)/templates:/var/www/html/templates \
--v $(pwd)/vendor:/var/www/html/vendor \
--v $(pwd)/.htaccess:/var/www/html/.htaccess \
--v $(pwd)/favicon.ico:/var/www/html/favicon.ico \
--v $(pwd)/index.php:/var/www/html/index.php \
-attack
+    -e XDEBUG_CONFIG="remote_host=192.168.99.1" \
+    -v $(pwd)/dist:/var/www/html/dist \
+    -v $(pwd)/php:/var/www/html/php \
+    -v $(pwd)/templates:/var/www/html/templates \
+    -v $(pwd)/vendor:/var/www/html/vendor \
+    -v $(pwd)/.htaccess:/var/www/html/.htaccess \
+    -v $(pwd)/favicon.ico:/var/www/html/favicon.ico \
+    -v $(pwd)/index.php:/var/www/html/index.php \
+    attack
 
 # optionally install linked php-my-admin which can be reached at port 8080
 docker run --name attack-pma --link attack-db:mysql -d -p 8080:80 \
--e PMA_HOST=attack-db \
-phpmyadmin/phpmyadmin
+    -e PMA_HOST=attack-db \
+    phpmyadmin/phpmyadmin
