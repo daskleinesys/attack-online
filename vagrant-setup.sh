471cbde1f8b81b5a321e388ea194dfaa511da102
Tom Schagerl <tom@thespielplatz.com>
2017-01-29 22:40:14 +0100

    vagrant setup bugfix

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -3,7 +3,7 @@
 echo "-- install php7.1 --"
 sudo apt-get update
 sudo add-apt-repository ppa:ondrej/php
-sudo apt-get install -y php7.1 php7.1-mysql libapache2-mod-php7.0 php-mbstring
+sudo apt-get install -y php7.1 php7.1-mysql libapache2-mod-php7.1 php-mbstring
 sudo apt-get update
 sudo a2dismod php5
 sudo a2enmod php7.1
@@ -30,5 +30,5 @@ mysql -uroot -proot -e "CREATE DATABASE attack;"
 mysql -uroot -proot -e "CREATE USER 'attack'@'localhost' IDENTIFIED BY 'attack';"
 mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON attack.* TO 'attack'@'localhost';FLUSH PRIVILEGES;"
 mysql -uattack -pattack attack < /var/www/mysqldumps/01_create.sql
-mysql -uattack -pattack attack < /var/www/mysqldumps/02_create.sql
+mysql -uattack -pattack attack < /var/www/mysqldumps/02_insert.sql
 mysql -uattack -pattack attack < /var/www/mysqldumps/local_user_games.sql
